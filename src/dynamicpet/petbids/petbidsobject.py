e45b52b4b3301f6d7a208048df679a6a879ad90a
Murat Bilgel <bilgelm@users.noreply.github.com>
2024-09-13 18:10:45 -0400

    Add decay correction and concatenation for PETBIDSObject (#119)
    feat: add decay correction and concatenation for PETBIDSObject
    
    add interrupted scan decay correction notebook
    add function to set TimeZero to ScanStart or InjectionStart
    bump up dynamicpet version

--- src/dynamicpet/petbids/petbidsobject.py
+++ src/dynamicpet/petbids/petbidsobject.py
@@ -1,13 +1,17 @@
 """PETBIDSObject abstract class."""
 
 from abc import ABC
+from typing import Literal
+from typing import Tuple
 
 import numpy as np
 
 from ..temporalobject.temporalobject import TemporalObject
 from ..typing_utils import NumpyNumberArray
 from .petbidsjson import PetBidsJson
+from .petbidsjson import get_hhmmss
 from .petbidsjson import get_radionuclide_halflife
+from .petbidsjson import timediff
 
 
 class PETBIDSObject(TemporalObject["PETBIDSObject"], ABC):
@@ -21,24 +25,133 @@ class PETBIDSObject(TemporalObject["PETBIDSObject"], ABC):
 
     json_dict: PetBidsJson
 
-    def get_decay_correction_factor(self) -> NumpyNumberArray:
-        """Get radionuclide decay correction factor."""
+    def set_timezero(
+        self, anchor: Literal["InjectionStart", "ScanStart"] = "InjectionStart"
+    ) -> None:
+        """Modify time tags and frame time start to specified anchor."""
+        if self.json_dict[anchor] == 0:
+            return
+
+        offset = self.json_dict[anchor]  # offset in seconds
+        frametimesstart = self.json_dict["FrameTimesStart"]
+        new_timezero = get_hhmmss(self.json_dict, anchor)
+
+        # update attribute
+        self.frame_start = self.frame_start - offset / 60
+
+        # update json tags
+        scalar_tags_to_update = [
+            "InjectionStart",
+            "ScanStart",
+            "ImageDecayCorrectionTime",
+            "InjectionEnd",
+        ]
+        for tag in scalar_tags_to_update:
+            if tag in self.json_dict:
+                self.json_dict[tag] -= offset  # type: ignore
+
+        self.json_dict["FrameTimesStart"] = [fts - offset for fts in frametimesstart]
+        self.json_dict["TimeZero"] = new_timezero.strftime("%H:%M:%S")
+
+    def get_decay_correction_factor(
+        self, decaycorrecttime: float = 0
+    ) -> NumpyNumberArray:
+        """Get radionuclide decay correction factor.
+
+        Args:
+            decaycorrecttime: time offset (in seconds) relative to TimeZero for
+                              decay correction factor calculation
+
+        Returns:
+            decay correction factors
+        """
         halflife = get_radionuclide_halflife(self.json_dict)
         lmbda = np.log(2) / halflife
         lmbda_dt = lmbda * self.frame_duration
-        factor = -np.exp(lmbda * self.frame_start) * lmbda_dt / np.expm1(-lmbda_dt)
+        factor = (
+            -np.exp(lmbda * (self.frame_start - decaycorrecttime / 60))
+            * lmbda_dt
+            / np.expm1(-lmbda_dt)
+        )
         return np.array(factor)
 
-    def get_decay_corrected_tacs(self) -> NumpyNumberArray:
-        """Decay correct time activity curves to time zero."""
+    def get_decay_corrected_tacs(self, decaycorrecttime: float = 0) -> NumpyNumberArray:
+        """Decay correct time activity curves (TACs).
+
+        Args:
+            decaycorrecttime: new time offset (in seconds) to decay correct to,
+                              relative to TimeZero
+
+        Returns:
+            decay corrected TACs
+        """
         # check if tacs are already decay corrected
-        # TODO
-        factor = self.get_decay_correction_factor()
-        return self.dataobj * factor
+        if (
+            self.json_dict["ImageDecayCorrected"]
+            and decaycorrecttime == self.json_dict["ImageDecayCorrectionTime"]
+        ):
+            return self.dataobj
+
+        factor = self.get_decay_correction_factor(decaycorrecttime)
+        return self.get_decay_uncorrected_tacs() * factor
 
     def get_decay_uncorrected_tacs(self) -> NumpyNumberArray:
-        """Decay uncorrect TACs (assuming decay correction was to time zero)."""
+        """Decay uncorrect time activity curves (TACs)."""
         # check if tacs are already decay uncorrected
-        # TODO
-        factor = self.get_decay_correction_factor()
+        if not self.json_dict["ImageDecayCorrected"]:
+            return self.dataobj
+
+        factor = self.get_decay_correction_factor(
+            self.json_dict["ImageDecayCorrectionTime"]
+        )
         return self.dataobj / factor
+
+    def _decay_correct_offset(
+        self, other: "PETBIDSObject"
+    ) -> Tuple[float, Literal["InjectionStart", "ScanStart"]]:
+        """Calculate ImageDecayCorrectionTime offset needed to match other to self.
+
+        This is a helper function for concatenate.
+
+        Args:
+            other: PETBIDSObject to be adjusted, if needed
+
+        Returns:
+            newdecaycorrecttime: new decay correction time relative to time zero
+                                 of self, in seconds
+            original_anchor: anchor time of self
+
+        Raises:
+            ValueError: radionuclides or injection/scan times are incompatible
+        """
+        # check if scans are combineable
+        # - verify same radionuclide
+        if (
+            self.json_dict["TracerRadionuclide"]
+            != other.json_dict["TracerRadionuclide"]
+        ):
+            raise ValueError("Radionuclides are incompatible")
+
+        # - verify same injection time
+        if get_hhmmss(self.json_dict, "InjectionStart") != get_hhmmss(
+            other.json_dict, "InjectionStart"
+        ):
+            raise ValueError("Injection times are incompatible")
+
+        # - check scan timing
+        this_firstframestart = get_hhmmss(self.json_dict, "FirstFrameStart")
+        other_firstframestart = get_hhmmss(other.json_dict, "FirstFrameStart")
+        if timediff(other_firstframestart, this_firstframestart) <= self.total_duration:
+            raise ValueError("Scan times are incompatible")
+
+        original_anchor: Literal["InjectionStart", "ScanStart"]
+        if self.json_dict["InjectionStart"] == 0:
+            original_anchor = "InjectionStart"
+        else:
+            original_anchor = "ScanStart"
+            self.set_timezero(anchor="InjectionStart")
+
+        other.set_timezero(anchor="InjectionStart")
+
+        newdecaycorrecttime = self.json_dict["ImageDecayCorrectionTime"]
+        return newdecaycorrecttime, original_anchor
--- tests/test_petbidsjson.py
+++ tests/test_petbidsjson.py
@@ -5,31 +5,105 @@ import pytest
 
 from dynamicpet.petbids.petbidsjson import PetBidsJson
 from dynamicpet.petbids.petbidsjson import get_frametiming_in_mins
+from dynamicpet.petbids.petbidsjson import get_hhmmss
 from dynamicpet.petbids.petbidsjson import get_radionuclide_halflife
+from dynamicpet.petbids.petbidsjson import timediff
 
 
-def test_get_frametiming_from_json_scanstart0() -> None:
-    """Test getting frame_start and frame_end from json when scan start is 0."""
+def test_get_hhmmss_scanstart0() -> None:
+    """Test ScanStart, InjectionStart, ImageDecayCorrectionTime in HH:MM:SS."""
     my_json_dict: PetBidsJson = {
+        "TimeZero": "10:00:00",
+        "InjectionStart": -120,
+        "ScanStart": 0,
+        "FrameTimesStart": [60, 180, 300],
+        "FrameDuration": [120, 120, 120],
+        "TracerRadionuclide": "C11",
+        "ImageDecayCorrected": True,
+        "ImageDecayCorrectionTime": -60,
+    }
+    assert "10:00:00" == get_hhmmss(my_json_dict, "ScanStart").isoformat()
+    assert "10:01:00" == get_hhmmss(my_json_dict, "FirstFrameStart").isoformat()
+    assert "09:58:00" == get_hhmmss(my_json_dict, "InjectionStart").isoformat()
+    assert (
+        "09:59:00" == get_hhmmss(my_json_dict, "ImageDecayCorrectionTime").isoformat()
+    )
+
+
+def test_get_hhmmss_injstart0() -> None:
+    """Test ScanStart, InjectionStart, ImageDecayCorrectionTime in HH:MM:SS."""
+    my_json_dict: PetBidsJson = {
+        "TimeZero": "09:58:00",
+        "InjectionStart": 0,
+        "ScanStart": 120,
+        "FrameTimesStart": [180, 300, 420],
+        "FrameDuration": [120, 120, 120],
+        "TracerRadionuclide": "C11",
+        "ImageDecayCorrected": True,
+        "ImageDecayCorrectionTime": 60,
+    }
+    assert "10:00:00" == get_hhmmss(my_json_dict, "ScanStart").isoformat()
+    assert "10:01:00" == get_hhmmss(my_json_dict, "FirstFrameStart").isoformat()
+    assert "09:58:00" == get_hhmmss(my_json_dict, "InjectionStart").isoformat()
+    assert (
+        "09:59:00" == get_hhmmss(my_json_dict, "ImageDecayCorrectionTime").isoformat()
+    )
+
+
+def test_timediff() -> None:
+    """Test timediff."""
+    my_json_dict: PetBidsJson = {
+        "TimeZero": "10:00:00",
         "InjectionStart": -120,
         "ScanStart": 0,
         "FrameTimesStart": [0, 120, 240],
         "FrameDuration": [120, 120, 120],
         "TracerRadionuclide": "C11",
+        "ImageDecayCorrected": True,
+        "ImageDecayCorrectionTime": -60,
+    }
+    assert (
+        timediff(
+            get_hhmmss(my_json_dict, "ScanStart"),
+            get_hhmmss(my_json_dict, "InjectionStart"),
+        )
+        == my_json_dict["ScanStart"] - my_json_dict["InjectionStart"]
+    )
+    assert (
+        timediff(
+            get_hhmmss(my_json_dict, "ImageDecayCorrectionTime"),
+            get_hhmmss(my_json_dict, "InjectionStart"),
+        )
+        == my_json_dict["ImageDecayCorrectionTime"] - my_json_dict["InjectionStart"]
+    )
+
+
+def test_get_frametiming_from_json_scanstart0() -> None:
+    """Test getting frame_start and frame_end from json when scan start is 0."""
+    my_json_dict: PetBidsJson = {
+        "TimeZero": "00:00:00",
+        "InjectionStart": -120,
+        "ScanStart": 0,
+        "FrameTimesStart": [0, 120, 240],
+        "FrameDuration": [120, 120, 120],
+        "TracerRadionuclide": "C11",
+        "ImageDecayCorrected": True,
     }
     frame_start, frame_duration = get_frametiming_in_mins(my_json_dict)
-    assert np.all(frame_start == np.array([120, 240, 360]) / 60)
+    assert np.all(frame_start == np.array([0, 120, 240]) / 60)
     assert np.all(frame_duration == np.array([120, 120, 120]) / 60)
 
 
 def test_get_frametiming_from_invalid_jsons() -> None:
     """Test getting frame_start and frame_end from invalid json."""
     my_json_dict: PetBidsJson = {
+        "TimeZero": "00:00:00",
         "InjectionStart": 1,
         "ScanStart": 1,
         "FrameTimesStart": [0, 120, 240],
         "FrameDuration": [120, 120, 120],
         "TracerRadionuclide": "C11",
+        "ImageDecayCorrected": True,
     }
 
     with pytest.raises(ValueError) as excinfo:
@@ -50,10 +124,12 @@ def test_get_frametiming_from_invalid_jsons() -> None:
 def test_halflife() -> None:
     """Test halflife for C11."""
     my_json_dict: PetBidsJson = {
+        "TimeZero": "00:00:00",
         "InjectionStart": -120,
         "ScanStart": 0,
         "FrameTimesStart": [0, 120, 240],
         "FrameDuration": [120, 120, 120],
         "TracerRadionuclide": "C11",
+        "ImageDecayCorrected": True,
     }
     assert get_radionuclide_halflife(my_json_dict) == 1224 / 60
