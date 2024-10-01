fix: align outputs with PET-BIDS derivatives

src/dynamicpet/__main__.py
@@ -3,7 +3,6 @@
 import csv
 import os
 import re
-import sys
 import warnings
 from json import dump as json_dump
 
@@ -112,11 +111,23 @@ def denoise(
     pet_img = petbidsimage_load(pet, json)
 
     if method == "HYPRLR":
+        doc = hypr.hypr_lr.__doc__
+
+        # parameters not relevant to HYPR-LR
+        mask = None
+        window_half_size = None
+        thresh = None
+
         if fwhm:
             res = hypr.hypr_lr(pet_img, fwhm)
         else:
             raise ValueError("fwhm must be specified for HYPR-LR")
     elif method == "NESMA":
+        doc = nesma.nesma_semiadaptive.__doc__
+
+        # parameter not relevant to NESMA
+        fwhm = None
+
         if mask:
             mask_img: SpatialImage = nib_load(mask)  # type: ignore
             # check that mask is in the same space as pet
@@ -147,7 +158,31 @@ def denoise(
         # maintain compatibility with the PET-BIDS Derivatives convention.
         froot = re.sub("_pet$", "", os.path.join(outputdir, bname))
     output = froot + "_desc-" + method.lower() + "_pet" + ext + addext
+    output_json = froot + "_desc-" + method.lower() + "_pet.json"
     res.to_filename(output)
+
+    cmd = (
+        f"denoise --method {method} "
+        + (f"--fwhm {fwhm} " if fwhm else "")
+        + (f"--mask {mask} " if mask else "")
+        + (f"--window_half_size {window_half_size} " if window_half_size else "")
+        + (f"--thresh {thresh} " if thresh else "")
+        + (f"--outputdir {outputdir} " if outputdir else "")
+        + (f"--json {json} " if json else "")
+        + pet
+    )
+    derivative_json_dict = {
+        "Description": (
+            re.sub(r"\s+", " ", doc.split("Args:")[0]).strip() if doc else ""
+        ),
+        # "Sources": [pet],
+        "SoftwareName": "dynamicpet",
+        "SoftwareVersion": __version__,
+        "CommandLine": cmd,
+    }
+
+    with open(output_json, "w") as f:
+        json_dump(derivative_json_dict, f, indent=4)
 
 
 @click.command()
@@ -305,12 +340,14 @@ def kineticmodel(  # noqa: C901
     if outputdir:
         os.makedirs(outputdir, exist_ok=True)
         bname = os.path.basename(froot)
-        # if the input file name follows the PET-BIDS convention, it should end
-        # with "_pet". Need to remove this to maintain compatibility with the
-        # PET-BIDS Derivatives convention.
-        froot = re.sub("_pet$", "", os.path.join(outputdir, bname))
+        froot = os.path.join(outputdir, bname)
 
     if isinstance(pet_img, PETBIDSMatrix):
+        # if the input file name follows the PET-BIDS Derivatives convention,
+        # it should end with "_tacs". Need to remove this to maintain
+        # compatibility with the PET-BIDS Derivatives convention.
+        froot = re.sub("_tacs$", "", froot)
+
         output = froot + "_model-" + model_abbr + "_kinpar" + ext
         output_json = froot + "_model-" + model_abbr + "_kinpar.json"
         data = np.empty((len(km.parameters), pet_img.num_elements))
@@ -323,6 +360,11 @@ def kineticmodel(  # noqa: C901
             for i, elem in enumerate(pet_img.elem_names):
                 tsvwriter.writerow([elem] + datat[i].tolist())
     else:
+        # if the input file name follows the PET-BIDS convention, it should end
+        # with "_pet". Need to remove this to maintain compatibility with the
+        # PET-BIDS Derivatives convention.
+        froot = re.sub("_pet$", "", froot)
+
         # save estimated parameters as image
         for param in km.parameters.keys():
             res_img: SpatialImage = km.get_parameter(param)  # type: ignore
@@ -336,10 +378,8 @@ def kineticmodel(  # noqa: C901
                 + ext
                 + addext
             )
-            output_json = (
-                froot + "_model-" + model_abbr + "_meas-" + param + "_mimap.json"
-            )
             res_img.to_filename(output)
+        output_json = froot + "_model-" + model_abbr + "_mimap.json"
 
     # save json PET BIDS derivative file
     inputvalues = [start, end]
@@ -354,25 +394,39 @@ def kineticmodel(  # noqa: C901
         inputvalueslabels += ["Full width at half max"]
         inputvaluesunits += ["mm"]
 
+    cmd = (
+        f"kineticmodel --model {model} "
+        + (f"--refroi {refroi} " if refroi else f"--refmask {refmask} ")
+        + (f"--outputdir {outputdir} " if outputdir else "")
+        + (f"--json {json} " if json else "")
+        + (f"--petmask {petmask} " if petmask else "")
+        + f"--start {start} "
+        + f"--end {end} "
+        + (f"--fwhm {fwhm} " if fwhm else "")
+        + f"--weight_by {weight_by} "
+        + f"--integration_type {integration_type} "
+        + pet
+    )
+    doc = km.__class__.__doc__
     derivative_json_dict = {
-        "Description": km.__class__.__doc__,
+        "Description": re.sub(r"\s+", " ", doc) if doc else "",
         # "Sources": [pet],
-        "ModelName": "SRTM",
+        "ModelName": model_abbr,
         "ReferenceRegion": refroi if refroi else refmask,
         "AdditionalModelDetails": (
-            f"Frame weighting by: {weight_by}.",
-            f"Integration type: {integration_type}",
+            f"Frame weighting by: {weight_by}. "
+            + f"Integration type: {integration_type}.",
         ),
         "InputValues": inputvalues,
         "InputValuesLabels": inputvalueslabels,
         "InputValuesUnits": inputvaluesunits,
         "SoftwareName": "dynamicpet",
         "SoftwareVersion": __version__,
-        "CommandLine": " ".join(sys.argv),
+        "CommandLine": cmd,
     }
 
     with open(output_json, "w") as f:
-        json_dump(derivative_json_dict, f)
+        json_dump(derivative_json_dict, f, indent=4)
 
 
 def parse_kineticmodel_inputs(

src/dynamicpet/kineticmodel/srtm.py
@@ -30,7 +30,7 @@ class SRTMLammertsma1996(KineticModel):
     @classmethod
     def get_param_names(cls) -> list[str]:
         """Get names of kinetic model parameters."""
-        return ["BP_ND", "R1", "k2"]
+        return ["BPND", "R1", "k2"]
 
     def fit(
         self,
@@ -71,7 +71,7 @@ class SRTMLammertsma1996(KineticModel):
             )
             bp_nd[k], r1[k], k2[k] = popt
 
-        self.set_parameter("BP_ND", bp_nd, mask)
+        self.set_parameter("BPND", bp_nd, mask)
         self.set_parameter("R1", r1, mask)
         self.set_parameter("k2", k2, mask)
 
@@ -82,7 +82,7 @@ class SRTMLammertsma1996(KineticModel):
 
         for i in trange(num_elements):
             idx = np.unravel_index(i, self.tacs.shape[:-1])
-            bp_nd = self.parameters["BP_ND"][*idx]
+            bp_nd = self.parameters["BPND"][*idx]
             r1 = self.parameters["R1"][*idx]
             k2 = self.parameters["k2"][*idx]
             if bp_nd or r1 or k2:
@@ -161,11 +161,10 @@ class SRTMZhou2003(KineticModel):
             "R1",
             "k2",
             "k2a",
-            "R1_lrsc",
-            "k2_lrsc",
-            "k2a_lrsc",
-            "noise_var_eq_dvr",
-            "noise_var_eq_r1",
+            "R1LRSC",
+            "k2LRSC",
+            "k2aLRSC",
+            "noiseVarEqR1",
         ]
 
     def fit(  # noqa: max-complexity: 12
@@ -281,8 +280,7 @@ class SRTMZhou2003(KineticModel):
         self.set_parameter("R1", r1, mask)
         self.set_parameter("k2", k2, mask)
         self.set_parameter("k2a", k2a, mask)
-        self.set_parameter("noise_var_eq_dvr", noise_var_eq_dvr, mask)
-        self.set_parameter("noise_var_eq_r1", noise_var_eq_r1, mask)
+        self.set_parameter("noiseVarEqR1", noise_var_eq_r1, mask)
 
         if do_smooth:
             smooth_r1_mat, smooth_k2_mat, smooth_k2a_mat, h = self.prep_refine_r1(
@@ -315,9 +313,9 @@ class SRTMZhou2003(KineticModel):
 
                 r1_lrsc[k], k2_lrsc[k], k2a_lrsc[k] = b
 
-            self.set_parameter("R1_lrsc", r1_lrsc, mask)
-            self.set_parameter("k2_lrsc", k2_lrsc, mask)
-            self.set_parameter("k2a_lrsc", k2a_lrsc, mask)
+            self.set_parameter("R1LRSC", r1_lrsc, mask)
+            self.set_parameter("k2LRSC", k2_lrsc, mask)
+            self.set_parameter("k2aLRSC", k2a_lrsc, mask)
 
     def prep_refine_r1(
         self,
@@ -351,7 +349,7 @@ class SRTMZhou2003(KineticModel):
             self.get_parameter("k2a"), fwhm  # type: ignore
         )
 
-        noise_var_eq_r1_img: SpatialImage = self.get_parameter("noise_var_eq_r1")  # type: ignore
+        noise_var_eq_r1_img: SpatialImage = self.get_parameter("noiseVarEqR1")  # type: ignore
         r1_img: SpatialImage = self.get_parameter("R1")  # type: ignore
         k2_img: SpatialImage = self.get_parameter("k2")  # type: ignore
         k2a_img: SpatialImage = self.get_parameter("k2a")  # type: ignore
@@ -409,7 +407,7 @@ class SRTMZhou2003(KineticModel):
         num_elements = self.tacs.num_elements
         fitted_tacs_dataobj = np.empty_like(self.tacs.dataobj)
 
-        use_lrsc = "R1_lrsc" in self.parameters
+        use_lrsc = "R1LRSC" in self.parameters
 
         for i in trange(num_elements):
             idx = np.unravel_index(i, self.tacs.shape[:-1])
@@ -419,8 +417,8 @@ class SRTMZhou2003(KineticModel):
             if dvr or r1 or k2:
                 bp_nd = dvr - 1
                 if use_lrsc:
-                    r1 = self.parameters["R1_lrsc"][*idx]
-                    k2 = self.parameters["k2_lrsc"][*idx]
+                    r1 = self.parameters["R1LRSC"][*idx]
+                    k2 = self.parameters["k2LRSC"][*idx]
                 fitted_tacs_dataobj[*idx, :] = srtm_model(self.reftac, bp_nd, r1, k2)
 
         if isinstance(self.tacs, TemporalImage):

tests/test_kineticmodel.py
@@ -101,7 +101,7 @@ def test_srtm_zhou2003_ti(reftac: TemporalMatrix, tacs_img: TemporalImage) -> No
 
     assert dvr_img.shape == (1, 1, 2)
 
-    bp_nd_img: SpatialImage = km.get_parameter("BP_ND")  # type: ignore
+    bp_nd_img: SpatialImage = km.get_parameter("BPND")  # type: ignore
 
     assert np.allclose(dvr_img.get_fdata(), bp_nd_img.get_fdata() + 1)
 

tests/test_kineticmodel_simulated.py
@@ -194,7 +194,7 @@ def test_srtmzhou2003_tm(
 
     fitted_tacs = km.fitted_tacs()
 
-    bp_nd: NumpyRealNumberArray = km.get_parameter("BP_ND")  # type: ignore
+    bp_nd: NumpyRealNumberArray = km.get_parameter("BPND")  # type: ignore
     r1: NumpyRealNumberArray = km.get_parameter("R1")  # type: ignore
 
     relative_tol = 0.007  # .007 means that 0.7% error is tolerated
@@ -232,9 +232,9 @@ def test_srtmzhou2003_ti(
 
     fitted_tacs = km.fitted_tacs()
 
-    bp_nd: SpatialImage = km.get_parameter("BP_ND")  # type: ignore
+    bp_nd: SpatialImage = km.get_parameter("BPND")  # type: ignore
     r1: SpatialImage = km.get_parameter("R1")  # type: ignore
-    r1_lrsc: SpatialImage = km.get_parameter("R1_lrsc")  # type: ignore
+    r1_lrsc: SpatialImage = km.get_parameter("R1LRSC")  # type: ignore
 
     relative_tol = 0.007  # .007 means that 0.7% error is tolerated
     assert np.allclose(bp_nd.get_fdata(), bp_nd_true, rtol=relative_tol)
@@ -262,7 +262,7 @@ def test_srtmlammertsma1996_tm(
 
     fitted_tacs = km.fitted_tacs()
 
-    bp_nd: NumpyRealNumberArray = km.get_parameter("BP_ND")  # type: ignore
+    bp_nd: NumpyRealNumberArray = km.get_parameter("BPND")  # type: ignore
     r1: NumpyRealNumberArray = km.get_parameter("R1")  # type: ignore
 
     relative_tol = 0.02  # .02 means that 2% error is tolerated
@@ -290,7 +290,7 @@ def test_lrtm_tm(
     # to fit to obtain the best possible recovery of true values
     km.fit(integration_type="trapz")
 
-    bp_nd: NumpyRealNumberArray = km.get_parameter("BP_ND")  # type: ignore
+    bp_nd: NumpyRealNumberArray = km.get_parameter("BPND")  # type: ignore
 
     relative_tol = 0.02  # .02 means that 2% error is tolerated
     assert np.allclose(bp_nd, bp_nd_true, rtol=relative_tol)

tests/test_main.py
@@ -1,7 +1,6 @@
 """Test cases for __main__.py."""
 
 import csv
-import os
 from pathlib import Path
 
 import numpy as np
@@ -32,9 +31,9 @@ def images() -> dict[str, Path]:
     outdir = Path(__file__).parent / "test_data"
     outdir.mkdir(exist_ok=True)
 
-    rois_fname = outdir / "displacementROI_dseg.nii.gz"
-    petjson_fname = outdir / "pet.json"
-    pet_fname = outdir / "pet.nii"
+    rois_fname = outdir / "sub-000101_ses-baseline_label-displacementROI_dseg.nii.gz"
+    petjson_fname = outdir / "sub-000101_ses-baseline_pet.json"
+    pet_fname = outdir / "sub-000101_ses-baseline_pet.nii"
 
     baseurl = "https://s3.amazonaws.com/openneuro.org/ds001705/sub-000101/ses-baseline/"
     roisurl = (
@@ -72,9 +71,9 @@ def images() -> dict[str, Path]:
                 for chunk in r.iter_content(chunk_size=8192):
                     f.write(chunk)
 
-    petmask_fname = outdir / "petmask.nii.gz"
-    refmask_fname = outdir / "refmask.nii.gz"
-    rois_csv_fname = outdir / "rois.tsv"
+    petmask_fname = outdir / "sub-000101_ses-baseline_desc-pet_mask.nii.gz"
+    refmask_fname = outdir / "sub-000101_ses-baseline_desc-ref_mask.nii.gz"
+    rois_csv_fname = outdir / "sub-000101_ses-baseline_tacs.tsv"
 
     if petmask_fname.exists() and refmask_fname.exists() and rois_csv_fname.exists():
         pass
@@ -142,7 +141,8 @@ def test_denoise_hyprlr(images: dict[str, Path]) -> None:
     )
 
     assert result.exit_code == 0
-    assert os.path.isfile(outputdir / "pet_desc-hyprlr_pet.nii")
+    assert (outputdir / "sub-000101_ses-baseline_desc-hyprlr_pet.nii").is_file()
+    assert (outputdir / "sub-000101_ses-baseline_desc-hyprlr_pet.json").is_file()
 
 
 def test_denoise_nesma(images: dict[str, Path]) -> None:
@@ -175,7 +175,8 @@ def test_denoise_nesma(images: dict[str, Path]) -> None:
     )
 
     assert result.exit_code == 0
-    assert os.path.isfile(outputdir / "pet_desc-nesma_pet.nii")
+    assert (outputdir / "sub-000101_ses-baseline_desc-nesma_pet.nii").is_file()
+    assert (outputdir / "sub-000101_ses-baseline_desc-nesma_pet.json").is_file()
 
 
 def test_kineticmodel_suvr(images: dict[str, Path]) -> None:
@@ -208,7 +209,7 @@ def test_kineticmodel_suvr(images: dict[str, Path]) -> None:
         catch_exceptions=False,
     )
     assert result.exit_code == 0
-    assert (outputdir / "rois_model-SUVR_kinpar.tsv").is_file()
+    assert (outputdir / "sub-000101_ses-baseline_model-SUVR_kinpar.tsv").is_file()
 
     # next, test with nifti images
     pet_fname = images["pet_fname"]
@@ -237,13 +238,16 @@ def test_kineticmodel_suvr(images: dict[str, Path]) -> None:
         catch_exceptions=False,
     )
     assert result.exit_code == 0
-    assert os.path.isfile(outputdir / "pet_model-SUVR_meas-SUVR_mimap.nii")
+    assert (
+        outputdir / "sub-000101_ses-baseline_model-SUVR_meas-SUVR_mimap.nii"
+    ).is_file()
+    assert (outputdir / "sub-000101_ses-baseline_model-SUVR_mimap.json").is_file()
 
     # finally, make sure that the two methods give the same results
 
     # skip the header (column names) and the first column (ROI names)
     rois_km_suvr = np.genfromtxt(
-        outputdir / "rois_model-SUVR_kinpar.tsv",
+        outputdir / "sub-000101_ses-baseline_model-SUVR_kinpar.tsv",
         delimiter="\t",
         skip_header=1,
         usecols=(1,),
@@ -254,7 +258,7 @@ def test_kineticmodel_suvr(images: dict[str, Path]) -> None:
 
     # calculate avg SUVR (pet_model-SUVR_meas-SUVR_mimap.nii) per ROI in rois_img
     pet_km_suvr_img: SpatialImage = nib_load(  # type: ignore
-        outputdir / "pet_model-SUVR_meas-SUVR_mimap.nii"
+        outputdir / "sub-000101_ses-baseline_model-SUVR_meas-SUVR_mimap.nii"
     )
     pet_km_suvr = pet_km_suvr_img.get_fdata()
     num_rois = len(np.unique(rois_img.get_fdata())) - 1
@@ -299,7 +303,8 @@ def test_kineticmodel_srtmzhou2003(images: dict[str, Path]) -> None:
         catch_exceptions=False,
     )
     assert result.exit_code == 0
-    assert os.path.isfile(outputdir / ("rois_model-SRTM_kinpar.tsv"))
+    assert (outputdir / "sub-000101_ses-baseline_model-SRTM_kinpar.tsv").is_file()
+    assert (outputdir / "sub-000101_ses-baseline_model-SRTM_kinpar.json").is_file()
 
     # next, test with nifti images
     pet_fname = images["pet_fname"]
@@ -328,5 +333,10 @@ def test_kineticmodel_srtmzhou2003(images: dict[str, Path]) -> None:
         catch_exceptions=False,
     )
     assert result.exit_code == 0
-    assert os.path.isfile(outputdir / ("pet_model-SRTM_meas-DVR_mimap.nii"))
-    assert os.path.isfile(outputdir / ("pet_model-SRTM_meas-R1_mimap.nii"))
+    assert (
+        outputdir / "sub-000101_ses-baseline_model-SRTM_meas-DVR_mimap.nii"
+    ).is_file()
+    assert (
+        outputdir / "sub-000101_ses-baseline_model-SRTM_meas-R1_mimap.nii"
+    ).is_file()
+    assert (outputdir / "sub-000101_ses-baseline_model-SRTM_mimap.json").is_file()