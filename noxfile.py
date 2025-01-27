e765b6c8be0df166ad778c5f8e41fec4835df4e7
Murat Bilgel <bilgelm@users.noreply.github.com>
2025-01-26 19:39:11 -0500

    remove safety scan from nox file

--- noxfile.py
+++ noxfile.py
@@ -138,21 +138,6 @@ def precommit(session: Session) -> None:
         activate_virtualenv_in_precommit_hooks(session)
 
 
-@session(python=python_versions[0])
-def safety(session: Session) -> None:
-    """Scan dependencies for insecure packages."""
-    requirements = session.poetry.export_requirements()
-    session.install("safety")
-    session.run(
-        "safety",
-        "scan",
-        "--full-report",
-        f"--file={requirements}",
-        "--ignore",
-        "70612",
-    )
-
-
 @session(python=python_versions)
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
